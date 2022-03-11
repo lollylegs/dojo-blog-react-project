6a3687c8ee32a349a379bbf1c617eb8924f02b30
MareaNorto <nortonmarea@gmail.com>
2022-03-11 14:30:35 +1030

    finish #11 Props

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import{ useState } from 'react';
-//import './index.css';
+import BlogList from './BlogList';
+
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -9,12 +10,7 @@ const Home = () => {
     ]);
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!"/>
         </div>
     );
 }
