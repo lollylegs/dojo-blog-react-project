c2bfc63cc4b1cd7f2263f633b6ab3bf6244c55ed
MareaNorto <nortonmarea@gmail.com>
2022-03-12 12:07:08 +1030

    finish #15 useEffect dependicies

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs);
@@ -16,14 +18,17 @@ const Home = () => {
 
     useEffect(() => {
         console.log('use effect ran');
-        console.log(blogs);
-    });
+        console.log(name)
+        }, [name]);
 
     return (
-        <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/>
-        </div>
+       <div className="home">
+           <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+           <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')}
+           title="Mario's blogs"/>
+           <button onClick={() => setName('luigi') }>change name</button>
+           <p>{ name }</p>
+       </div>
     );
 }
  
