64d627d8c978695e81f5f5cca75a994d6d3810d6
MareaNorto <nortonmarea@gmail.com>
2022-03-18 16:51:04 +1030

    finish cleanup of useEffect

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,25 +7,33 @@ const useFetch = (url) => {
    const [error, setError] = useState(null);
 
    useEffect (() => {
-      setTimeout(() => {
-         fetch(url)
-         .then(res => {
-             if(!res.ok) {
-                 throw Error('could not fetch data for that resouce')
-             }
-             return res.json();
-         })
-         .then(data => {
-             setData(data);
-             setIsPending(false);
-             setError(null);
-         })
-         .catch((err) => {
-             setIsPending(false);
-             setError(err.message);
-         })
-      }, 1000);
-   }, [url]);
+       const abortCont = new AbortController();
+
+       setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
+            .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch data for that resouce')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch((err) => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted')
+                }else {
+                    setIsPending(false);
+                    setError(err.message);
+                }
+            })
+        }, 1000);
+
+        return () => abortCont.abort();
+    }, [url]);
 
    return { data, isPending, error }
 }
